947bbf1f102c17eaa7ae10c980e39fcfed445ee0
Purushottam Kushwaha <pkushwah@codeaurora.org>
2021-09-15 14:58:20 +0530

    Wifi: Disconnect on IP_REACHABILITY_LOST for a specific period
    IP_REACHABILITY_LOST shall be triggered on a NUD_FAIL indication. The
    current logic to check for the IP Reachability is to wait for the
    response for the configured number of ARP requests to the gateway before
    triggering a NUD_FAILURE. This feature when enabled by default is resulting
    into some false positives (due to the ARP responses getting delayed from
    the AP), leading to a disconnection / reconnection to the network.
    
    Considering the pros / cons of this feature, this commit customizes this
    behavior to be active only for the first 10 seconds after the connect/roam
    to the AP. This should help to identify the roaming across multiple subnets.
    The new architectures have offloaded this IPReachabilityMonitor to the host
    driver which have more control of the Wi-Fi statistics before they trigger
    the disconnect. This should address any issue due to the data stalls.
    
    All said,this enhancement of "Disconnect on IP_REACHABILITY_LOST for a
    specific period" is enabled by default and controlled by the overlay
    "config_disconnectOnlyOnInitialIpReachability" .
    Set this to false if this enhancement is not required in device overlay.
    
    Change-Id: I64cbf358e8281556b5a02df26a62396996f88aca
    CRs-Fixed: 3028641

--- service/java/com/android/server/wifi/ClientModeImpl.java
+++ service/java/com/android/server/wifi/ClientModeImpl.java
@@ -265,6 +265,10 @@ public class ClientModeImpl extends StateMachine implements ClientMode {
     @Nullable
     private WifiNative.ConnectionCapabilities mLastConnectionCapabilities;
 
+    /* if set to true then disconnect due to IP Reachability lost only
+     * when obtained for the first 10 seconds of L2 connection */
+    private boolean mIpReachabilityMonitorActive = true;
+
     private String getTag() {
         return TAG + "[" + (mInterfaceName == null ? "unknown" : mInterfaceName) + "]";
     }
@@ -544,6 +548,9 @@ public class ClientModeImpl extends StateMachine implements ClientMode {
     static final int CMD_START_FILS_CONNECTION                          = BASE + 262;
 
     static final int CMD_CONNECTABLE_STATE_SETUP                        = BASE + 300;
+
+    /* Vendor specific cmd: To handle IP Reachability session */
+    private static final int CMD_IP_REACHABILITY_SESSION_END            = BASE + 311;
 
     /* Tracks if suspend optimizations need to be disabled by DHCP,
      * screen or due to high perf mode.
@@ -1936,6 +1943,11 @@ public class ClientModeImpl extends StateMachine implements ClientMode {
                 break;
             case WifiMonitor.NETWORK_NOT_FOUND_EVENT:
                 sb.append(" ssid=" + msg.obj);
+                break;
+            case CMD_IP_REACHABILITY_SESSION_END:
+                if (msg.obj != null) {
+                    sb.append(" ").append((String) msg.obj);
+                }
                 break;
             default:
                 sb.append(" ");
@@ -3837,6 +3849,7 @@ public class ClientModeImpl extends StateMachine implements ClientMode {
                 case CMD_START_ROAM:
                 case CMD_START_RSSI_MONITORING_OFFLOAD:
                 case CMD_STOP_RSSI_MONITORING_OFFLOAD:
+                case CMD_IP_REACHABILITY_SESSION_END:
                 case CMD_IP_CONFIGURATION_SUCCESSFUL:
                 case CMD_IP_CONFIGURATION_LOST:
                 case CMD_IP_REACHABILITY_LOST: {
@@ -4399,6 +4412,7 @@ public class ClientModeImpl extends StateMachine implements ClientMode {
                             mPasspointManager.requestVenueUrlAnqpElement(scanResult);
                         }
                     }
+                    mIpReachabilityMonitorActive = true;
                     transitionTo(mL3ProvisioningState);
                     break;
                 }
@@ -4993,6 +5007,10 @@ public class ClientModeImpl extends StateMachine implements ClientMode {
                             WifiUsabilityStats.LABEL_BAD,
                             WifiUsabilityStats.TYPE_IP_REACHABILITY_LOST, -1);
                     if (mWifiGlobals.getIpReachabilityDisconnectEnabled()) {
+                        if (mWifiGlobals.getDisconnectOnlyOnInitialIpReachability() && !mIpReachabilityMonitorActive) {
+                            logd("CMD_IP_REACHABILITY_LOST Connect session is over, skip ip reachability lost indication.");
+                            break;
+                        }
                         handleIpReachabilityLost();
                     } else {
                         logd("CMD_IP_REACHABILITY_LOST but disconnect disabled -- ignore");
@@ -5025,6 +5043,8 @@ public class ClientModeImpl extends StateMachine implements ClientMode {
                         clearTargetBssid("AllowlistRoamingCompleted");
                         sendNetworkChangeBroadcast(DetailedState.CONNECTED);
                     }
+                    mIpReachabilityMonitorActive = true;
+                    sendMessageDelayed(obtainMessage(CMD_IP_REACHABILITY_SESSION_END, 0, 0), 10000);
                     break;
                 }
                 case CMD_ONESHOT_RSSI_POLL: {
@@ -5457,6 +5477,7 @@ public class ClientModeImpl extends StateMachine implements ClientMode {
                         //
                         // mIpClient.confirmConfiguration() is called within
                         // the handling of SupplicantState.COMPLETED.
+                        mIpReachabilityMonitorActive = true;
                         transitionTo(mL3ConnectedState);
                     } else {
                         mMessageHandlingStatus = MESSAGE_HANDLING_STATUS_DISCARD;
@@ -5513,6 +5534,10 @@ public class ClientModeImpl extends StateMachine implements ClientMode {
             mWifiConnectivityManager.handleConnectionStateChanged(
                     mClientModeManager,
                     WifiConnectivityManager.WIFI_STATE_CONNECTED);
+
+            if (mIpReachabilityMonitorActive)
+                sendMessageDelayed(obtainMessage(CMD_IP_REACHABILITY_SESSION_END, 0, 0), 10000);
+
             registerConnected();
             mTargetWifiConfiguration = null;
             mWifiScoreReport.reset();
@@ -5727,6 +5752,10 @@ public class ClientModeImpl extends StateMachine implements ClientMode {
                     handleStatus = NOT_HANDLED;
                     break;
                 }
+                case CMD_IP_REACHABILITY_SESSION_END: {
+                    mIpReachabilityMonitorActive = false;
+                    break;
+                }
                 default: {
                     handleStatus = NOT_HANDLED;
                     break;
@@ -5764,6 +5793,9 @@ public class ClientModeImpl extends StateMachine implements ClientMode {
                 mWifiP2pConnection.sendMessage(WifiP2pServiceImpl.DISCONNECT_WIFI_RESPONSE);
                 return;
             }
+
+            mIpReachabilityMonitorActive = false;
+            removeMessages(CMD_IP_REACHABILITY_SESSION_END);
 
             if (mVerboseLoggingEnabled) {
                 logd(" Enter DisconnectedState screenOn=" + mScreenOn);
